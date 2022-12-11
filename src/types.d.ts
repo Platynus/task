20249be902dcf8fc823d34ffaf24e0b41837f63d
Patryk <patrykpawelec2002@gmail.com>
2022-12-11 16:25:10 +0100

    Seting Types

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,6 +4,12 @@
 
 export type Breadcrumb = {
   // TODO
+  Film: Films[];
+  People: People[];
+  Planet: Planets[];
+  Spieces: Spieces[];
+  Starship: Starships[];
+  Vehicle: Vehicles[];
 };
 
 export type Movie = {
@@ -19,6 +25,68 @@ export type Character = {
   height: number;
   mass: string;
   birth_year: string;
+  films: string[];
+  url: string;
+};
+
+export type Film = {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  url: string;
+};
+export type Planet = {
+  name: string;
+  rotation_period: number;
+  orbital_period: number;
+  diameter: number;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: number;
+  population: number;
+  residents: string[];
+  films: string[];
+  url: string;
+};
+export type Species = {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  skin_colors: string;
+  hair_colors: string;
+  eye_colors: string;
+  average_lifespan: string;
+  homeworld: string;
+  language: string;
+  people: string[];
+  films: string[];
+  url: string;
+};
+export type Starship = {
+  name: string;
+  model: string;
+  manufacturer: string;
+  cost_in_credits: string;
+  length: string;
+  max_atmosphering_speed: string;
+  crew: string;
+  passengers: string;
+  cargo_capacity: string;
+  consumables: string;
+  hyperdrive_rating: string;
+  MGLT: string;
+  starship_class: string;
+  pilots: string[];
   films: string[];
   url: string;
 };
